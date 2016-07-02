eff68be4d5da54c653821c67d98ee4f845feaf63
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-07-02 12:21:19 -0500

    Updated SwiftPM package

--- Package.swift
+++ Package.swift
@@ -2,6 +2,10 @@ import PackageDescription
 
 let package = Package(
     name: "Bluetooth",
+    targets: [
+        Target(
+            name: "Bluetooth")
+    ],
     dependencies: [
         .Package(url: "https://github.com/PureSwift/SwiftFoundation", majorVersion: 2)
     ]
