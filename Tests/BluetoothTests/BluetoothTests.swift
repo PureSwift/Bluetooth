1986cca05b852b4bec2b7b398f5c61abc91ee007
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-12-03 19:41:28 -0500

    Added unit tests

--- Tests/BluetoothTests/BluetoothTests.swift
+++ Tests/BluetoothTests/BluetoothTests.swift
@@ -13,7 +13,8 @@ import Foundation
 final class BluetoothTests: XCTestCase {
     
     static let allTests = [
-        ("testAddress", testAddress)
+        ("testAddress", testAddress),
+        ("testUUID", testUUID)
         ]
     
     func testAddress() {
@@ -35,11 +36,25 @@ final class BluetoothTests: XCTestCase {
         
         do {
             
-            /// refersed C7A8D570-E023-4FB8-E511-72F9E24FF160
+            let uuidValue = GATT.UUID.PrimaryService.rawValue // 0x2800
+            let uuid = BluetoothUUID.bit16(uuidValue)
+            
+            XCTAssert(uuid.rawValue == "2800")
+            XCTAssert(uuid.littleEndian == [0x00, 0x28])
+            XCTAssert(uuid.littleEndian != [0x28, 0x00])
+            
+        }
+        
+        do {
+            
+            /// reversed C7A8D570-E023-4FB8-E511-72F9E24FF160
             let uuidString = "60F14FE2-F972-11E5-B84F-23E070D5A8C7"
             let uuid = BluetoothUUID.bit128(UUID(rawValue: uuidString)!)
             
             XCTAssert(uuid.rawValue == uuidString)
+            XCTAssert(uuid.littleEndian == [0xC7, 0xA8, 0xD5, 0x70, 0xE0, 0x23, 0x4F, 0xB8, 0xE5, 0x11, 0x72, 0xF9, 0xE2, 0x4F, 0xF1, 0x60])
+            //XCTAssert([UInt8](uuid.data) == [0x60, 0xF1, 0x4F, 0xE2, 0xF9, 0x72, 0x11, 0xE5, 0xB8, 0x4F, 0x23, 0xE0, 0x70, 0xD5, 0xA8, 0xC7])
+            //XCTAssert(BluetoothUUID.bit128(UUID(data: Data([0x60, 0xF1, 0x4F, 0xE2, 0xF9, 0x72, 0x11, 0xE5, 0xB8, 0x4F, 0x23, 0xE0, 0x70, 0xD5, 0xA8, 0xC7]))!) == BluetoothUUID.bit128(UUID(rawValue: "60F14FE2-F972-11E5-B84F-23E070D5A8C7")!))
         }
     }
 }
