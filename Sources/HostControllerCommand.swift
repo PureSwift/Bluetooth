c027d91a729a6b133a1121bbe72b9dc4398d28ec
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-03-23 15:50:07 -0500

    Added `LinkPolicyCommand`

---
+++ Sources/HostControllerCommand.swift
@@ -0,0 +1,9 @@
+//
+//  HostControllerCommand.swift
+//  Bluetooth
+//
+//  Created by Alsey Coleman Miller on 3/23/18.
+//  Copyright © 2018 PureSwift. All rights reserved.
+//
+
+import Foundation
---
+++ Sources/TestingCommand.swift
@@ -0,0 +1,9 @@
+//
+//  TestingCommand.swift
+//  Bluetooth
+//
+//  Created by Alsey Coleman Miller on 3/23/18.
+//  Copyright © 2018 PureSwift. All rights reserved.
+//
+
+import Foundation
