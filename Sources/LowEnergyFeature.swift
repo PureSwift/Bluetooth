5cc781ba18d3406d73b8fd3caa528c026ecad67e
jmarkstar <mestrella.1991@gmail.com>
2018-03-29 18:45:32 -0500

    Added `ReadRemoteUsedFeaturesCompleteParameter`

--- Sources/LowEnergyFeature.swift
+++ Sources/LowEnergyFeature.swift
@@ -18,14 +18,73 @@
  */
 public enum LowEnergyFeature: UInt16, BitMaskOption {
     
+    public typealias RawValue = 
+    
     /// LE Encryption
     case encryption                                     = 0b0000000000000001
     
     /// Connection Parameters Request Procedure
     case connectionParametersRequestProcedure           = 0b0000000000000010
     
+    /// Extended Reject Indication
+    case extendedRejectIndication                       = 0b0000000000000100
+    
+    /// Slave-initiated Features Exchange
+    case slaveInitiatedFeaturesExchange                 = 0b0000000000001000
+    
+    /// LE Ping
+    case lePing                                         = 0b0000000000010000
+    
+    /// LE Data Packet Length Extension
+    case leDataPacketLengthExtension                    = 0b0000000000100000
+    
+    /// LL Privacy
+    case elPrivacy                                      = 0b0000000001000000
+    
+    /// Extended Scanner Filter Policies
+    case extendedScannerFilterPolicies                  = 0b0000000010000000
+    
+    /// LE 2M PHY
+    case le2mPhy                                        = 0b0000000100000000
+    
+    /// Stable Modulation Index - Transmitter
+    case stableModulationIndexTransmitter               = 0b0000001000000000
+    
+    /// Stable Modulation Index - Receiver
+    case stableModulationIndexReceiver                  = 0b0000010000000000
+    
+    /// LE Coded PHY
+    case leCodedPhy                                     = 0b0000100000000000
+    
+    /// LE Extended Advertising
+    case leExtendedAdvertising                          = 0b0001000000000000
+    
+    /// LE Periodic Advertising
+    case lePeriodicAdvertising                          = 0b0010000000000000
+    
+    /// Channel Selection Algorithm #2
+    case channelSelectionAlgorithm2                     = 0b0100000000000000
+    
+    ///LE Power Class 1
+    case lePowerClass1                                  = 0b1000000000000000
+    
     public static let all: Set<LowEnergyFeature> = [.encryption,
-                                                    .connectionParametersRequestProcedure]
+                                                    .connectionParametersRequestProcedure,
+                                                    .extendedRejectIndication,
+                                                    .slaveInitiatedFeaturesExchange,
+                                                    .lePing,
+                                                    .leDataPacketLengthExtension,
+                                                    .leDataPacketLengthExtension,
+                                                    .elPrivacy,
+                                                    .extendedScannerFilterPolicies,
+                                                    .le2mPhy,
+                                                    .stableModulationIndexTransmitter,
+                                                    .stableModulationIndexReceiver,
+                                                    .leCodedPhy,
+                                                    .leExtendedAdvertising,
+                                                    .lePeriodicAdvertising,
+                                                    .channelSelectionAlgorithm2,
+                                                    .lePowerClass1]
 }
 
 // MARK: - CustomStringConvertible
@@ -72,7 +131,21 @@ public extension LowEnergyFeature {
 internal let featureSet: [LowEnergyFeature: (isValid: Bool, name: String)] = [
     
     .encryption: (true, "LE Encryption"),
-    .connectionParametersRequestProcedure: (true, "Connection Parameters Request Procedure")
+    .connectionParametersRequestProcedure: (true, "Connection Parameters Request Procedure"),
+    .extendedRejectIndication: (true, "Extended Reject Indication"),
+    .slaveInitiatedFeaturesExchange: (true, "Slave-initiated Features Exchange"),
+    .lePing: (false, "LE Ping"),
+    .leDataPacketLengthExtension: (true, "LE Data Packet Length Extension"),
+    .elPrivacy: (false, "LL Privacy"),
+    .extendedScannerFilterPolicies: (true, "Extended Scanner Filter Policies"),
+    .le2mPhy: (true, "LE 2M PHY"),
+    .stableModulationIndexTransmitter: (true, "Stable Modulation Index - Transmitter"),
+    .stableModulationIndexReceiver: (true, "Stable Modulation Index - Receiver"),
+    .leCodedPhy: (true, "LE Coded PHY"),
+    .leExtendedAdvertising: (false, "LE Extended Advertising"),
+    .lePeriodicAdvertising: (false, "LE Periodic Advertising"),
+    .channelSelectionAlgorithm2: (true, "Channel Selection Algorithm #2"),
+    .lePowerClass1: (true, "LE Power Class 1")
 ]
 
 // MARK: - Supporting Types
