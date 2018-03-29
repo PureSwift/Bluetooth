4dce8177f6e7aa6f2c63a2e5bd789e72672997ff
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-03-29 08:46:30 -0500

    Fixed Linux XCTest compilation error
    Missing `XCTAssertNoThrow()` on Linux with Swift 3

--- Tests/BluetoothTests/HCITests.swift
+++ Tests/BluetoothTests/HCITests.swift
@@ -18,7 +18,8 @@ final class HCITests: XCTestCase {
         ("testAdvertisingReport", testAdvertisingReport),
         ("testCommandStatusEvent", testCommandStatusEvent),
         ("testLEConnection", testLEConnection),
-        ("testWriteLocalName", testWriteLocalName)
+        ("testWriteLocalName", testWriteLocalName),
+        ("testReadLocalName", testReadLocalName)
     ]
     
     func testReadLocalName() {
@@ -271,3 +272,17 @@ fileprivate func parseEvent <T: HCIEventParameter> (_ actualBytesRead: Int, _ ev
     
     return event
 }
+
+// MARK: - XCTest Linux Support
+
+#if swift(>=3.2)
+#elseif os(Linux) && swift(>=3.0)
+public func XCTAssertNoThrow<T>(_ expression: @autoclosure () throws -> T, _ message: @autoclosure () -> String = "", file: StaticString = #file, line: UInt = #line) {
+    
+    do { let _ = try expression() }
+    catch {
+        print("threw error \"\(error)\"")
+        XCTFail(message(), file: file, line: line)
+    }
+}
+#endif
