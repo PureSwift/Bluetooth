1fcebe038475620d5b97239dcc0213779a71a278
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-03-31 06:55:40 -0500

    Updated `BluetoothHostControllerInterface`

--- Sources/iBeacon.swift
+++ Sources/iBeacon.swift
@@ -66,12 +66,12 @@ public func iBeacon(uuid: Foundation.UUID,
 public extension BluetoothHostControllerInterface {
     
     /// Enable iBeacon functionality.
-    mutating func iBeacon(uuid: Foundation.UUID = UUID(),
-                          major: UInt16,
-                          minor: UInt16,
-                          rssi: Int8,
-                          interval: UInt16 = 100,
-                          timeout: Int = HCI.defaultTimeout) throws {
+    func iBeacon(uuid: Foundation.UUID = UUID(),
+                 major: UInt16,
+                 minor: UInt16,
+                 rssi: Int8,
+                 interval: UInt16 = 100,
+                 timeout: Int = HCI.defaultTimeout) throws {
         
         // set advertising parameters
         let advertisingParameters = LowEnergyCommand.SetAdvertisingParametersParameter(interval: (interval, interval))
