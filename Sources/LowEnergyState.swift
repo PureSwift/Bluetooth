33ad345b7507080d437a265651b994be29d07825
jmarkstar <mestrella.1991@gmail.com>
2018-04-04 11:01:58 -0700

    Finished `LowEnergyState`

--- Sources/LowEnergyState.swift
+++ Sources/LowEnergyState.swift
@@ -10,21 +10,39 @@
 public enum LowEnergyState: UInt8, BitMaskOption {
     
     /// Scannable Advertising State
-    case scannableAdvertising = 0b01 // 0x0001  // LE_STATE_SCAN_ADV
+    case scannableAdvertising                   = 0b01 // 0x0001  // LE_STATE_SCAN_ADV
     
     /// Connectable Advertising State
-    case connectableAdvertising = 0b10 // 0x0002  // LE_STATE_CONN_ADV
+    case connectableAdvertising                 = 0b10 // 0x0002  // LE_STATE_CONN_ADV
     
     /// Non-Connectable Advertising State
-    case nonConnectableAdvertising = 0b100 // 0x0004 // LE_STATE_NONCONN_ADV
+    case nonConnectableAdvertising              = 0b100 // 0x0004 // LE_STATE_NONCONN_ADV
     
     /// High Duty Cycle Directed Advertising State
-    case highDutyCycleDirectedAdvertising = 0b1000 // LE_STATE_HIGH_DIRECT_ADV 0x0008
+    case highDutyCycleDirectedAdvertising       = 0b1000 // LE_STATE_HIGH_DIRECT_ADV 0x0008
+    
+    case lowDutyCycleDirectedAsvertising        = 0b1010 // LE_STATE_LOW_DIRECT_ADV 0x0010
+    
+    case activeScanning                         = 0b10100 // LE_STATE_ACTIVE_SCAN 0x0020
+    
+    case passiveScanning                        = 0b101000 // LE_STATE_PASSIVE_SCAN 0x0040
+    
+    case initiating                             = 0b1010000 // LE_STATE_INITIATING 0x0080
+    
+    case connectionMaster                       = 0b1100100 // LE_STATE_MASTER_MASTER 0x0100
+    
+    case connectionSlave                        = 0b11001000 // LE_STATE_SLAVE_SLAVE 0x0200
     
     public static let all: Set<LowEnergyState> = [.scannableAdvertising,
                                                   .connectableAdvertising,
                                                   .nonConnectableAdvertising,
-                                                  .highDutyCycleDirectedAdvertising]
+                                                  .highDutyCycleDirectedAdvertising,
+                                                  .lowDutyCycleDirectedAsvertising,
+                                                  .activeScanning,
+                                                  .passiveScanning,
+                                                  .initiating,
+                                                  .connectionMaster,
+                                                  .connectionSlave]
 }
 
 public extension LowEnergyState {
@@ -49,10 +67,48 @@ extension LowEnergyState: CustomStringConvertible {
 /// The states and state combinations that the link layer supports.
 public enum LowEnergyStateSet: UInt64 {
     
-    case state0 = 0b01
-    case state1 = 0b10
-    case state2 = 0b100
-    case state3 = 0b1000
+    case state0  = 0b01
+    case state1  = 0b10
+    case state2  = 0b100
+    case state3  = 0b1000
+    case state4  = 0b10000
+    case state5  = 0b100000
+    case state6  = 0b1000000
+    case state7  = 0b10000000
+    case state8  = 0b100000000
+    case state9  = 0b1000000000
+    case state10 = 0b10000000000
+    case state11 = 0b100000000000
+    case state12 = 0b1000000000000
+    case state13 = 0b10000000000000
+    case state14 = 0b100000000000000
+    case state15 = 0b1000000000000000
+    case state16 = 0b10000000000000000
+    case state17 = 0b100000000000000000
+    case state18 = 0b1000000000000000000
+    case state19 = 0b10000000000000000000
+    case state20 = 0b100000000000000000000
+    case state21 = 0b1000000000000000000000
+    case state22 = 0b10000000000000000000000
+    case state23 = 0b100000000000000000000000
+    case state24 = 0b1000000000000000000000000
+    case state25 = 0b10000000000000000000000000
+    case state26 = 0b100000000000000000000000000
+    case state27 = 0b1000000000000000000000000000
+    case state28 = 0b10000000000000000000000000000
+    case state29 = 0b100000000000000000000000000000
+    case state30 = 0b1000000000000000000000000000000
+    case state31 = 0b10000000000000000000000000000000
+    case state32 = 0b100000000000000000000000000000000
+    case state33 = 0b1000000000000000000000000000000000
+    case state34 = 0b10000000000000000000000000000000000
+    case state35 = 0b100000000000000000000000000000000000
+    case state36 = 0b1000000000000000000000000000000000000
+    case state37 = 0b10000000000000000000000000000000000000
+    case state38 = 0b100000000000000000000000000000000000000
+    case state39 = 0b1000000000000000000000000000000000000000
+    case state40 = 0b10000000000000000000000000000000000000000
+    case state41 = 0b100000000000000000000000000000000000000000
 }
 
 // MARK: - Values
@@ -75,22 +131,57 @@ internal let lowEnergyStates: [LowEnergyStateSet: (BitMaskOptionSet<LowEnergySta
     .state0: ([.nonConnectableAdvertising]),
     .state1: ([.scannableAdvertising]),
     .state2: ([.connectableAdvertising]),
-    .state3: ([.highDutyCycleDirectedAdvertising])
+    .state3: ([.highDutyCycleDirectedAdvertising]),
+    .state4: ([.passiveScanning]),
+    .state5: ([.activeScanning]),
+    .state6: ([.initiating]),
+    .state7: ([.connectionSlave]),
+    .state8: ([.nonConnectableAdvertising, .passiveScanning]),
+    .state9: ([.scannableAdvertising, .passiveScanning]),
+    .state10: ([.connectableAdvertising, .passiveScanning]),
+    .state11: ([.highDutyCycleDirectedAdvertising ,.passiveScanning]),
+    .state12: ([.nonConnectableAdvertising, .activeScanning]),
+    .state13: ([.scannableAdvertising, .activeScanning]),
+    .state14: ([.connectableAdvertising, .activeScanning]),
+    .state15: ([.highDutyCycleDirectedAdvertising, .activeScanning]),
+    .state16: ([.nonConnectableAdvertising, .initiating]),
+    .state17: ([.scannableAdvertising, .initiating]),
+    .state18: ([.nonConnectableAdvertising, .connectionMaster]),
+    .state19: ([.scannableAdvertising, .connectionMaster]),
+    .state20: ([.nonConnectableAdvertising, .connectionSlave]),
+    .state21: ([.scannableAdvertising, .connectionSlave]),
+    .state22: ([.passiveScanning, .initiating]),
+    .state23: ([.activeScanning, .initiating]),
+    .state24: ([.passiveScanning, .connectionMaster]),
+    .state25: ([.activeScanning, .connectionMaster]),
+    .state26: ([.passiveScanning, .connectionSlave]),
+    .state27: ([.activeScanning, .connectionSlave]),
+    .state28: ([.initiating, .connectionMaster]),
+    .state29: ([.lowDutyCycleDirectedAsvertising]),
+    .state30: ([.lowDutyCycleDirectedAsvertising, .passiveScanning]),
+    .state31: ([.lowDutyCycleDirectedAsvertising, .activeScanning]),
+    .state32: ([.connectableAdvertising, .initiating]),
+    .state33: ([.highDutyCycleDirectedAdvertising, .initiating]),
+    .state34: ([.lowDutyCycleDirectedAsvertising, .initiating]),
+    .state35: ([.connectableAdvertising, .connectionMaster]),
+    .state36: ([.highDutyCycleDirectedAdvertising, .connectionMaster]),
+    .state37: ([.lowDutyCycleDirectedAsvertising, .connectionMaster]),
+    .state38: ([.connectableAdvertising, .connectionSlave]),
+    .state39: ([.highDutyCycleDirectedAdvertising, .connectionSlave]),
+    .state40: ([.lowDutyCycleDirectedAsvertising, .connectionSlave]),
+    .state41: ([.initiating, .connectionSlave])
+    
 ]
 
 internal let lowEnergyStateNames: [LowEnergyState: String] = [
-    .scannableAdvertising: "Scannable Advertising State",
-    .connectableAdvertising: "Connectable Advertising State",
-    .nonConnectableAdvertising: "Non-connectable Advertising State",
-    .highDutyCycleDirectedAdvertising: "High Duty Cycle Directed Advertising State"
-    /*
-{  4, "Low Duty Cycle Directed Advertising State"
-{  5, "Active Scanning State"
-{  6, "Passive Scanning State"
-{  7, "Initiating State"
-{  8, "Connection State (Master Role)"
-{  9, "Connection State (Slave Role)"
-{ 10, "Master Role & Master Role"
-{ 11, "Slave Role & Slave Role"
-{ 12, "Master Role & Slave Role"*/
+    .scannableAdvertising            : "Scannable Advertising State",
+    .connectableAdvertising          : "Connectable Advertising State",
+    .nonConnectableAdvertising       : "Non-connectable Advertising State",
+    .highDutyCycleDirectedAdvertising: "High Duty Cycle Directed Advertising State",
+    .lowDutyCycleDirectedAsvertising : "Low Duty Cycle Directed Advertising State",
+    .activeScanning                  : "Active Scanning State",
+    .passiveScanning                 : "Passive Scanning State",
+    .initiating                      : "Initiating State",
+    .connectionMaster                : "Connection State(Master Role)",
+    .connectionSlave                 : "Connection State(Slave Role)"
 ]
