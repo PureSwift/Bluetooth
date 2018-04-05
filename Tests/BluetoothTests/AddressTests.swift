35378623dc720e2dcd13213fd30f05ae5ccab897
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-04-04 20:18:14 -0500

    Updated Address unit tests

--- Tests/BluetoothTests/AddressTests.swift
+++ Tests/BluetoothTests/AddressTests.swift
@@ -13,8 +13,57 @@ import Foundation
 final class AddressTests: XCTestCase {
     
     static let allTests = [
-        ("testMalformedString", testMalformedString)
+        ("testBytes", testBytes),
+        ("testProperties", testProperties),
+        ("testMalformedString", testMalformedString),
+        ("testString", testString),
+        ("testData", testData),
+        ("testMalformedData", testMalformedData)
     ]
+    
+    func testProperties() {
+        
+        XCTAssertEqual(Address.bitWidth, MemoryLayout<Address.ByteValue>.size * 8)
+        XCTAssertEqual(Address.bitWidth, 48)
+        
+        Address.min.data.forEach { XCTAssertEqual($0, .min) }
+        Address.max.data.forEach { XCTAssertEqual($0, .max) }
+        Address.zero.data.forEach { XCTAssertEqual($0, 0) }
+        XCTAssertEqual(Address.zero, Address.min)
+        XCTAssertEqual(Address.zero, Address.any)
+        XCTAssertEqual(Address.max, Address.none)
+        
+        guard let address = Address(rawValue: "00:1A:7D:DA:71:13")
+            else { XCTFail("Could not parse"); return }
+        
+        XCTAssertNotEqual(address.hashValue, 0)
+        XCTAssertEqual(address.description, "00:1A:7D:DA:71:13")
+    }
+    
+    func testBytes() {
+        
+        let testData: [(rawValue: String, bytes: Address.ByteValue)] = [
+            ("00:1A:7D:DA:71:13", (0x00, 0x1A, 0x7D, 0xDA, 0x71, 0x13)),
+            ("59:80:ED:81:EE:35", (0x59, 0x80, 0xED, 0x81, 0xEE, 0x35)),
+            ("AC:BC:32:A6:67:42", (0xAC, 0xBC, 0x32, 0xA6, 0x67, 0x42))
+        ]
+        
+        for test in testData {
+            
+            guard let address = Address(rawValue: test.rawValue)
+                else { XCTFail("Could not parse"); continue }
+            
+            XCTAssertEqual(address.rawValue, test.rawValue)
+            XCTAssertEqual(address, Address(bigEndian: Address(bytes: test.bytes)))
+            
+            XCTAssertEqual(address.bigEndian.bytes.0, test.bytes.0)
+            XCTAssertEqual(address.bigEndian.bytes.1, test.bytes.1)
+            XCTAssertEqual(address.bigEndian.bytes.2, test.bytes.2)
+            XCTAssertEqual(address.bigEndian.bytes.3, test.bytes.3)
+            XCTAssertEqual(address.bigEndian.bytes.4, test.bytes.4)
+            XCTAssertEqual(address.bigEndian.bytes.5, test.bytes.5)
+        }
+    }
     
     func testMalformedString() {
         
@@ -48,33 +97,6 @@ final class AddressTests: XCTestCase {
         rawValues.forEach { XCTAssertEqual(Address(rawValue: $0)?.rawValue, $0) }
     }
     
-    func testBytes() {
-        
-        let testData: [(rawValue: String, bytes: Address.ByteValue)] = [
-            ("00:1A:7D:DA:71:13", (0x00, 0x1A, 0x7D, 0xDA, 0x71, 0x13)),
-            ("59:80:ED:81:EE:35", (0x59, 0x80, 0xED, 0x81, 0xEE, 0x35)),
-            ("AC:BC:32:A6:67:42", (0xAC, 0xBC, 0x32, 0xA6, 0x67, 0x42))
-        ]
-        
-        for test in testData {
-            
-            guard let address = Address(rawValue: test.rawValue)
-                else { XCTFail("Could not parse"); continue }
-            
-            XCTAssertEqual(address.rawValue, test.rawValue)
-            XCTAssertEqual(address, Address(bigEndian: Address(bytes: test.bytes)))
-            
-            XCTAssertEqual(address.bigEndian.bytes.0, test.bytes.0)
-            XCTAssertEqual(address.bigEndian.bytes.1, test.bytes.1)
-            XCTAssertEqual(address.bigEndian.bytes.2, test.bytes.2)
-            XCTAssertEqual(address.bigEndian.bytes.3, test.bytes.3)
-            XCTAssertEqual(address.bigEndian.bytes.4, test.bytes.4)
-            XCTAssertEqual(address.bigEndian.bytes.5, test.bytes.5)
-            
-            XCTAssertNotEqual(address.hashValue, 0)
-        }
-    }
-    
     func testData() {
         
         let testData: [(rawValue: String, data: Data)] = [
@@ -92,5 +114,20 @@ final class AddressTests: XCTestCase {
             XCTAssertEqual(address, Address(bigEndian: Address(data: test.data)!))
             XCTAssertEqual(address.bigEndian.data, test.data)
         }
+    }
+    
+    func testMalformedData() {
+        
+        let malformed = [
+            Data(),
+            Data([0x00]),
+            Data([0x00, 0x1A]),
+            Data([0x00, 0x1A, 0x7D]),
+            Data([0x00, 0x1A, 0x7D, 0xDA]),
+            Data([0x00, 0x1A, 0x7D, 0xDA, 0x71]),
+            Data([0x00, 0x1A, 0x7D, 0xDA, 0x71, 0x13, 0xAA])
+        ]
+        
+        malformed.forEach { XCTAssertNil(Address(data: $0)) }
     }
 }
