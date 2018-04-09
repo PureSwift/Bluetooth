59d5aad18698f9a02e79c83047ad9f9a6829ede7
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-04-09 07:23:07 -0500

    #27 Updated unit tests

--- Tests/BluetoothTests/iBeaconTests.swift
+++ Tests/BluetoothTests/iBeaconTests.swift
@@ -96,9 +96,71 @@ final class iBeaconTests: XCTestCase {
     
     func testCommand() {
         
-        let beacon = iBeacon(uuid: UUID(), major: 0, minor: 0, rssi: -55, interval: 200)
+        let uuid = UUID(rawValue: "E2C56DB5-DFFB-48D2-B060-D0F5A71096E0")!
+        
+        let beacon = iBeacon(uuid: uuid, major: 1, minor: 1, rssi: -29, interval: 100)
         
         let hostController = TestHostController()
+        
+        // LE Set Advertising Parameters (15 bytes)
+        hostController.queue.append(
+            .command(LowEnergyCommand.setAdvertisingParameters.opcode,
+                     [6, 32, 15, 100, 0, 100, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0])
+        )
+        
+        /**
+         RECV  Command Complete [2006] - LE Set Advertising Parameters
+         Parameter Length: 4 (0x04)
+         Status: 0x00 - Success
+         Num HCI Command Packets: 0x01
+         Opcode: 0x2006 (OGF: 0x08    OCF: 0x06) - [Low Energy] LE Set Advertising Parameters
+         */
+        hostController.queue.append(.event([0x0e, 0x04, 0x01, 0x06, 0x20, 0x00]))
+        
+        /**
+         SEND  [200A] LE Set Advertise Enable - 0x01
+         [200A] Opcode: 0x200A (OGF: 0x08    OCF: 0x0A)
+         Parameter Length: 1 (0x01)
+         Advertising Enable: 01
+         */
+        
+        hostController.queue.append(
+            .command(LowEnergyCommand.setAdvertiseEnable.opcode,
+            [0x0a, 0x20, 0x01, 0x01]
+            )
+        )
+        
+        /**
+         RECV  Command Complete [200A] - LE Set Advertise Enable
+         Parameter Length: 4 (0x04)
+         Status: 0x00 - Success
+         Num HCI Command Packets: 0x01
+         Opcode: 0x200A (OGF: 0x08    OCF: 0x0A) - [Low Energy] LE Set Advertise Enable
+         */
+        hostController.queue.append(.event([0x0e, 0x04, 0x01, 0x0a, 0x20, 0x00]))
+        
+        /**
+         SEND  [2008] LE Set Advertising Data
+         [2008] Opcode: 0x2008 (OGF: 0x08    OCF: 0x08)
+         Parameter Length: 32 (0x20)
+         Advertising Data Length: 1E
+         Flags: 0X1A
+         Apple Manufacturing Data
+         Length: 26 (0x1A)
+         */
+        hostController.queue.append(
+            .command(LowEnergyCommand.setAdvertisingData.opcode, [0x08, 0x20, 0x20, 0x1E, 0x02, 0x01, 0x1A, 0x1A, 0xFF, 0x4C, 0x00, 0x02, 0x15, 0xE2, 0xC5, 0x6D, 0xB5, 0xDF, 0xFB, 0x48, 0xD2, 0xB0, 0x60, 0xD0, 0xF5, 0xA7, 0x10, 0x96, 0xE0, 0x00, 0x01, 0x00, 0x01, 0xE3, 0x00]
+            )
+        )
+        
+        /**
+         Command Complete [2008] - LE Set Advertising Data
+         Parameter Length: 4 (0x04)
+         Status: 0x00 - Success
+         Num HCI Command Packets: 0x01
+         Opcode: 0x2008 (OGF: 0x08    OCF: 0x08) - [Low Energy] LE Set Advertising Data
+         */
+        hostController.queue.append(.event([0x0E, 0x04, 0x01, 0x08, 0x20, 0x00]))
         
         XCTAssertNoThrow(try hostController.iBeacon(beacon))
     }
