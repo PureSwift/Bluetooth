80f16f17d160bf4264bc91aa3369f551b83248ec
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-05-22 21:00:33 -0500

    #35 Fixed GAPCompleteListOf128BitServiceClassUUIDs, GAPIncompleteListOf128BitServiceClassUUIDs

--- Sources/GenericAccessProfile.swift
+++ Sources/GenericAccessProfile.swift
@@ -1,4 +1,3 @@
-
 //
 //  GenericAccessProfile.swift
 //  Bluetooth
@@ -670,12 +669,12 @@ public struct GAPIncompleteListOf128BitServiceClassUUIDs: GAPData {
         guard let list = Bit128UUIDList(data: data)
             else { return nil }
         
-        self.uuids = list.uuids
+        self.uuids = list.uuids.map(UUID.init)
     }
     
     public var data: Data {
         
-        return Bit128UUIDList(uuids: uuids).data
+        return Bit128UUIDList(uuids: uuids.map(UInt128.init)).data
     }
 }
 
@@ -720,12 +719,12 @@ public struct GAPCompleteListOf128BitServiceClassUUIDs: GAPData {
         guard let list = Bit128UUIDList(data: data)
             else { return nil }
         
-        self.uuids = list.uuids
+        self.uuids = list.uuids.map(UUID.init)
     }
     
     public var data: Data {
         
-        return Bit128UUIDList(uuids: uuids).data
+        return Bit128UUIDList(uuids: uuids.map(UInt128.init)).data
     }
 }
 
@@ -755,16 +754,16 @@ extension GAPCompleteListOf128BitServiceClassUUIDs: CustomStringConvertible {
 
 internal struct Bit128UUIDList {
     
-    public var uuids: [UUID]
+    public var uuids: [UInt128]
     
-    public init(uuids: [UUID]) {
+    public init(uuids: [UInt128]) {
         
         self.uuids = uuids
     }
     
     public init?(data: Data) {
         
-        var uuids = [UUID]()
+        var uuids = [UInt128]()
         uuids.reserveCapacity(data.count / 16)
         
         var index = 0
@@ -773,7 +772,7 @@ internal struct Bit128UUIDList {
             guard index + 15 < data.count
                 else { return nil }
             
-            let value = UUID(bytes: (data[index], data[index + 1], data[index + 2], data[index + 3], data[index + 4], data[index + 5], data[index + 6], data[index + 7], data[index + 8], data[index + 9], data[index + 10], data[index + 11], data[index + 12], data[index + 13], data[index + 14], data[index + 15]))
+            let value = UInt128(littleEndian: UInt128(bytes: (data[index], data[index + 1], data[index + 2], data[index + 3], data[index + 4], data[index + 5], data[index + 6], data[index + 7], data[index + 8], data[index + 9], data[index + 10], data[index + 11], data[index + 12], data[index + 13], data[index + 14], data[index + 15])))
             
             index += 16
             
@@ -785,7 +784,7 @@ internal struct Bit128UUIDList {
     
     public var data: Data {
         
-        return uuids.reduce(Data(), { $0.0 + [$0.1.bytes.0, $0.1.bytes.1, $0.1.bytes.2, $0.1.bytes.3, $0.1.bytes.4, $0.1.bytes.5, $0.1.bytes.6, $0.1.bytes.7, $0.1.bytes.8, $0.1.bytes.9, $0.1.bytes.10, $0.1.bytes.11, $0.1.bytes.12, $0.1.bytes.13, $0.1.bytes.14, $0.1.bytes.15] })
+        return uuids.reduce(Data(), { $0.0 + [$0.1.littleEndian.bytes.0, $0.1.littleEndian.bytes.1, $0.1.littleEndian.bytes.2, $0.1.littleEndian.bytes.3, $0.1.littleEndian.bytes.4, $0.1.littleEndian.bytes.5, $0.1.littleEndian.bytes.6, $0.1.littleEndian.bytes.7, $0.1.littleEndian.bytes.8, $0.1.littleEndian.bytes.9, $0.1.littleEndian.bytes.10, $0.1.littleEndian.bytes.11, $0.1.littleEndian.bytes.12, $0.1.littleEndian.bytes.13, $0.1.littleEndian.bytes.14, $0.1.littleEndian.bytes.15] })
     }
 }
 
