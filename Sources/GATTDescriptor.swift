b3049ecb2c650fb9e0d2ecdc6186d0d10354b17d
Jurj <jurjdev@gmail.com>
2018-05-25 14:47:23 -0500

    Added GATTAggregateFormatDescriptor

--- Sources/GATTDescriptor.swift
+++ Sources/GATTDescriptor.swift
@@ -206,7 +206,7 @@ public struct GATTServerCharacteristicConfiguration: GATTDescriptor {
     
     public var serverConfiguration: BitMaskOptionSet<ServerConfiguration>
     
-    public init(serverConfiguration: BitMaskOptionSet<ServerConfiguration> = []){
+    public init(serverConfiguration: BitMaskOptionSet<ServerConfiguration> = []) {
         
         self.serverConfiguration = serverConfiguration
     }
@@ -253,4 +253,48 @@ extension GATTServerCharacteristicConfiguration {
     }
 }
 
-
+/// GATT Characteristic Aggregate Format Descriptor
+///
+/// The Characteristic Aggregate Format descriptor defines the format of an aggregated Characteristic Value.
+///
+/// Only one Characteristic Aggregate Format descriptor exists in a characteristic definition.
+/// This descriptor consists of a list of Attribute Handles pointing to Characteristic Presentation Format declarations.
+/// This descriptor is read only and does not require authentication or authorization.
+/// The list of Attribute Handles is the concatenation of multiple 16-bit Attribute Handle values into a single Attribute Value.
+/// If more than one Characteristic Presentation Format declarations exist, then there is one Characteristic Aggregate Format declaration.
+/// However, a Characteristic Aggregate Format descriptor can be present even if there aren't any Presentation Format descriptors in the characteristic definition. The order of the Attribute Handles in the list is significant.
+///
+/// Example:
+/// If 3 Characteristic Presentation Format declarations exist at Attribute Handles 0x40, 0x50 and 0x60,
+/// the Characteris Aggregate Format Value is 0x405060.
+public struct GATTAggregateFormatDescriptor: GATTDescriptor {
+    
+    public static var uuid: BluetoothUUID = .characteristicAggregateFormat
+    
+    public var aggregateFormat: [UInt16]
+    
+    public init(aggregateFormat: [UInt16] = []) {
+        
+        self.aggregateFormat = aggregateFormat
+    }
+    
+    public init?(byteValue: Data) {
+        
+        guard let list = Bit16UUIDList(data: byteValue)
+            else { return nil }
+        self.aggregateFormat = list.uuids
+    }
+    
+    public var byteValue: Data {
+        
+        return (Bit16UUIDList(uuids: aggregateFormat)).data
+    }
+    
+    public var descriptor: GATT.Descriptor {
+    
+        return GATT.Descriptor(uuid: type(of: self).uuid,
+                               value: byteValue,
+                               permissions: [.read])
+    }
+    
+}
