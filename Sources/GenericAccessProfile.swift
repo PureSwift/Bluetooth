251ee0b837228a7d707abd4bdb436a7e293fdc8b
Carlos Duclos <darkzeratul64@gmail.com>
2018-05-28 11:16:39 -0500

    #35 added GAPURI and GAPLESupportedFeatures

--- Sources/GenericAccessProfile.swift
+++ Sources/GenericAccessProfile.swift
@@ -2011,6 +2011,152 @@ public enum GAPLERoleType: UInt8 {
     
 }
 
+/**
+ GAP Shortened Local Name
+ 
+ The Local Name data type shall be the same as, or a shortened version of, the local name assigned to the device. The Local Name data type value indicates if the name is complete or shortened. If the name is shortened, the complete name can be read using the remote name request procedure over BR/EDR or by reading the device name characteristic after the connection has been established using GATT.
+ 
+ A shortened name shall only contain contiguous characters from the beginning of the full name. For example, if the device name is ‘BT_Device_Name’ then the shortened name could be ‘BT_Device’ or ‘BT_Dev’.
+ */
+public struct GAPURI: GAPData {
+    
+    public static let dataType: GAPDataType = .uri
+    
+    public let name: String
+    
+    public init(name: String) {
+        
+        self.name = name
+    }
+    
+    public init?(data: Data) {
+        
+        guard let rawValue = String(data: data, encoding: .utf8)
+            else { return nil }
+        
+        self.init(name: rawValue)
+    }
+    
+    public var data: Data {
+        
+        return Data(name.utf8)
+    }
+}
+
+extension GAPURI: Equatable {
+    
+    public static func == (lhs: GAPURI, rhs: GAPURI) -> Bool {
+        
+        return lhs.name == rhs.name
+    }
+}
+
+extension GAPURI: CustomStringConvertible {
+    
+    public var description: String {
+        
+        return name
+    }
+}
+
+extension GAPURI: ExpressibleByStringLiteral {
+    
+    public init(stringLiteral value: String) {
+        
+        self.init(name: value)
+    }
+    
+    public init(extendedGraphemeClusterLiteral value: String) {
+        
+        self.init(name: value)
+    }
+    
+    public init(unicodeScalarLiteral value: String) {
+        
+        self.init(name: value)
+    }
+}
+
+/// The LE Supported Features data type defines the LE features supported by the device.
+/// All 0x00 octets after the last non-zero octet shall be omitted from the value transmitted.
+///
+/// The LE Supported Features data type size is zero or more octets long. This allows the LE Supported Features to be represented while using the minimum number of octets within the data packet.
+public struct GAPLESupportedFeatures: GAPData {
+    
+    public static let omittedValue: UInt8 = 0x00
+    
+    public static let dataType: GAPDataType = .LESupportedFeatures
+    
+    public let supportedFeatures: [UInt8]
+    
+    public init(supportedFeatures: [UInt8]) {
+        
+        self.supportedFeatures = supportedFeatures
+    }
+    
+    public init?(data: Data) {
+        
+        var supportedFeatures = [UInt8]()
+        var lastNonZero: UInt8?
+        
+        for i in 0..<data.count {
+            
+            let element = data[i]
+            
+            if lastNonZero != nil, element == GAPLESupportedFeatures.omittedValue {
+                continue
+            }
+            
+            supportedFeatures.append(element)
+            
+            if element != GAPLESupportedFeatures.omittedValue {
+                lastNonZero = element
+            }
+        }
+        
+        self.init(supportedFeatures: supportedFeatures)
+    }
+    
+    public var data: Data {
+        
+        var supportedData = Data()
+        var lastNonZero: UInt8?
+        
+        for i in 0..<supportedFeatures.count {
+            
+            let element = supportedFeatures[i]
+            
+            if lastNonZero != nil, element == GAPLESupportedFeatures.omittedValue {
+                continue
+            }
+            
+            supportedData += [element]
+            
+            if element != GAPLESupportedFeatures.omittedValue {
+                lastNonZero = element
+            }
+        }
+        
+        return supportedData
+    }
+}
+
+extension GAPLESupportedFeatures: Equatable {
+    
+    public static func == (lhs: GAPLESupportedFeatures, rhs: GAPLESupportedFeatures) -> Bool {
+        
+        return lhs.supportedFeatures == rhs.supportedFeatures
+    }
+}
+
+extension GAPLESupportedFeatures: CustomStringConvertible {
+    
+    public var description: String {
+        
+        return supportedFeatures.description
+    }
+}
+
 // MARK: - Coding
 
 public struct GAPDataElement {
