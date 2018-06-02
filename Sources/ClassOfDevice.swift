16bf4a8e47ef76a6b79b5ac09b229cd9aed650a5
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-06-01 19:53:46 -0500

    #35 Update GAP data types

---
+++ Sources/ClassOfDevice.swift
@@ -0,0 +1,39 @@
+//
+//  ClassOfDevice.swift
+//  Bluetooth
+//
+//  Created by Alsey Coleman Miller on 6/1/18.
+//  Copyright © 2018 PureSwift. All rights reserved.
+//
+/*
+public struct ClassOfDevice {
+    
+    public var major: Major
+    
+    public var minor: Minor
+}
+
+public extension ClassOfDevice {
+    
+    public struct Major: RawRepresentable {
+        
+        
+    }
+}
+
+public extension ClassOfDevice {
+    
+    public struct Major: RawRepresentable {
+        
+        
+    }
+}
+
+public extension ClassOfDevice {
+    
+    public struct Major: RawRepresentable {
+        
+        
+    }
+}
+*/
