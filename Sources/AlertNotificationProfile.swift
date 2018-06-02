a73ef92c39a63c9ef8b9efe5fff2da96aec64701
Carlos Duclos <darkzeratul64@gmail.com>
2018-06-01 20:16:46 -0500

    added AlertNotificationProfile
    added AlertNotificationProfile

---
+++ Sources/AlertNotificationProfile.swift
@@ -0,0 +1,15 @@
+//
+//  AlertNotificationProfile.swift
+//  Bluetooth
+//
+//  Created by Carlos Duclos on 6/1/18.
+//  Copyright © 2018 PureSwift. All rights reserved.
+//
+
+import Foundation
+
+public struct GATTAlertNotification {
+
+    
+
+}
