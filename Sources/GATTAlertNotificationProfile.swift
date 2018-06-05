d28971f7f62ae3ce04cb96da529966a86985d195
Carlos Duclos <darkzeratul64@gmail.com>
2018-06-05 00:25:47 -0500

    #43 added comments for alert notification service

--- Sources/GATTAlertNotificationProfile.swift
+++ Sources/GATTAlertNotificationProfile.swift
@@ -8,6 +8,20 @@
 
 import Foundation
 
+/**
+     Alert Notification service exposes:
+ 
+     • The different types of alerts with the short text messages.
+     • The information how many count of new alert messages.
+     • The information how many count of unread alerts.
+ 
+     The Alert Notification service exposes alert information in a device. This information includes the following:
+ 
+     - Type of alert occuring in a device.
+     - Additional text information such as caller ID or sender ID
+     - Count of new alerts.
+     - Count of unread alert items.
+ */
 public struct GATTAlertNotificationService: GATTProfileService {
     
     public static let UUID: BluetoothUUID = .supportedNewAlertCategory
