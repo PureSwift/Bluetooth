2eb698227d8dc7c04ac11290dab15e1485b656b6
Carlos Duclos <darkzeratul64@gmail.com>
2018-06-12 15:42:03 -0500

    #58 added GATTAlertLevel

--- Sources/GATTCharacteristic.swift
+++ Sources/GATTCharacteristic.swift
@@ -1443,6 +1443,51 @@ public struct GATTBloodPressureMeasurement: GATTProfileCharacteristic {
     }
 }
 
+/**
+ Alert Level
+ 
+ The level of an alert a device is to sound. If this level is changed while the alert is being sounded, the new level should take effect.
+ 
+ The value of the characteristic shall be an unsigned 8 bit integer that has a fixed point exponent of 0. The Alert Level characteristic defines the level of alert, and is one of the following three values:
+ 
+ • Value 0, meaning “No Alert”
+ 
+ • Value 1, meaning “Mild Alert”
+ 
+ • EValue 2, meaning “High Alert”
+ 
+ • Example:
+ 
+ The value 0x01 is interpreted as “Mild Alert”
+ 
+ - SeeAlso: [Alert Level](https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.characteristic.alert_level.xml)
+ */
+public enum GATTAlertLevel: UInt8, GATTProfileCharacteristic {
+    
+    public static var uuid: BluetoothUUID { return .alertLevel }
+    
+    internal static let length = MemoryLayout<UInt8>.size
+    
+    case noAlert = 0
+    
+    case mildAlert
+    
+    case highAlert
+    
+    public init?(data: Data) {
+        
+        guard data.count == type(of: self).length
+            else { return nil }
+        
+        self.init(rawValue: data[0])
+    }
+    
+    public var data: Data {
+        
+        return Data([rawValue])
+    }
+}
+
 // MARK: - Internal
 
 internal extension UInt64 {
