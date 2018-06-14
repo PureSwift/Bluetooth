ef75a9d1260c1f68cb2a69df2384f1f307b3a1a0
Carlos Duclos <darkzeratul64@gmail.com>
2018-06-14 13:44:14 -0500

    #72 Refactor source layout

---
+++ Sources/HCILESetResolvablePrivateAddressTimeout.swift
@@ -0,0 +1,9 @@
+//
+//  HCILESetResolvablePrivateAddressTimeout.swift
+//  Bluetooth
+//
+//  Created by Carlos Duclos on 6/14/18.
+//  Copyright © 2018 PureSwift. All rights reserved.
+//
+
+import Foundation
