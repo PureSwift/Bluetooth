33f64a62ff9995dc8169362b2748b7e41cca0144
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-06-14 03:25:34 -0500

    Swift 3 fixes

--- Sources/ATTReadByTypeRequest.swift
+++ Sources/ATTReadByTypeRequest.swift
@@ -50,14 +50,13 @@ public struct ATTReadByTypeRequest: ATTProtocolDataUnit {
             
         case .uuid16:
             
-            let value = UInt16(bytes: (data[5], data[6])).littleEndian
+            let value = UInt16(littleEndian: UInt16(bytes: (data[5], data[6])))
             
             self.attributeType = .bit16(value)
             
         case .uuid128:
             
-            self.attributeType = BluetoothUUID(littleEndian:
-                BluetoothUUID(data: Data([data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15], data[16], data[17], data[18], data[19], data[20]]))!)
+            self.attributeType = BluetoothUUID(littleEndian: BluetoothUUID(data: data.subdata(in: 5 ..< 21))!)
         }
     }
     
