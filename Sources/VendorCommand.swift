444ae4a422b67e16861151d8f089205e44fb5a98
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-06-15 13:10:17 -0500

    #72 Refactor source layout

--- Sources/VendorCommand.swift
+++ Sources/VendorCommand.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+public struct VendorCommand: HCICommand {
+    
+    public static let opcodeGroupField = HCIOpcodeGroupField.vendor
+    
+    public let rawValue: HCIOpcodeCommandField
+    
+    public init(rawValue: HCIOpcodeCommandField) {
+        
+        self.rawValue = rawValue
+    }
+    
+    public static var names = [VendorCommand: String]()
+    
+    public var name: String {
+        
+        return type(of: self).names[self] ?? rawValue.toHexadecimal()
+    }
+}
