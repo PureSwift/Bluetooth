5523eb80dfd0c3816251ccc9909079f62be260a4
Carlos Duclos <darkzeratul64@gmail.com>
2018-06-19 12:00:58 -0500

    #58 Implemented RawRepresentable on GATTBootKeyboardOutputReport

--- Sources/GATTBootKeyboardOutputReport.swift
+++ Sources/GATTBootKeyboardOutputReport.swift
@@ -15,17 +15,17 @@ import Foundation
  
  [Boot Keyboard Output Report](https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.characteristic.boot_keyboard_output_report.xml)
  */
-public struct GATTBootKeyboardOutputReport: GATTCharacteristic {
+public struct GATTBootKeyboardOutputReport: RawRepresentable, GATTCharacteristic {
     
     internal static let length = MemoryLayout<UInt8>.size
     
     public static var uuid: BluetoothUUID { return .bootKeyboardOutputReport }
     
-    public var value: UInt8
+    public var rawValue: UInt8
     
-    public init(value: UInt8) {
+    public init(rawValue: UInt8) {
         
-        self.value = value
+        self.rawValue = rawValue
     }
     
     public init?(data: Data) {
@@ -33,12 +33,12 @@ public struct GATTBootKeyboardOutputReport: GATTCharacteristic {
         guard data.count == type(of: self).length
             else { return nil }
         
-        self.init(value: data[0])
+        self.init(rawValue: data[0])
     }
     
     public var data: Data {
         
-        return Data([value])
+        return Data([rawValue])
     }
 }
 
@@ -46,7 +46,7 @@ extension GATTBootKeyboardOutputReport: Equatable {
     
     public static func == (lhs: GATTBootKeyboardOutputReport, rhs: GATTBootKeyboardOutputReport) -> Bool {
         
-        return lhs.value == rhs.value
+        return lhs.rawValue == rhs.rawValue
     }
 }
 
@@ -54,6 +54,6 @@ extension GATTBootKeyboardOutputReport: CustomStringConvertible {
     
     public var description: String {
         
-        return value.description
+        return rawValue.description
     }
 }
