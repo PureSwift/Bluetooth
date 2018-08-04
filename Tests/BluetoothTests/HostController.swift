fd76785b3729b1f49f3d004fccf68dabf31d9b6d
Carlos Duclos <darkzeratul64@gmail.com>
2018-08-04 00:49:26 -0500

    remove temporary extension

--- Tests/BluetoothTests/HostController.swift
+++ Tests/BluetoothTests/HostController.swift
@@ -367,15 +367,3 @@ extension Array {
         return first
     }
 }
-
-extension Data {
-    struct HexEncodingOptions: OptionSet {
-        let rawValue: Int
-        static let upperCase = HexEncodingOptions(rawValue: 1 << 0)
-    }
-    
-    func hexEncodedString(options: HexEncodingOptions = []) -> String {
-        let format = options.contains(.upperCase) ? "%02hhX" : "%02hhx"
-        return map { "0x" + String(format: format, $0) + ", " }.joined()
-    }
-}
