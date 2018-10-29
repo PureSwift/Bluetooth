6da60cbe5cc0c252c3b42fc02e407c5284733725
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-10-29 16:46:14 -0500

    Update for Swift 4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
 // swift-tools-version:4.1
 import PackageDescription
 
-_ = Package(name: "Bluetooth",
-            products: [
-                .library(
-                    name: "Bluetooth",
-                    targets: ["Bluetooth"]
-                )
-            ],
-            targets: [
-                .target(name: "Bluetooth", path: "./Sources"),
-                .testTarget(name: "BluetoothTests", dependencies: ["Bluetooth"])
-            ],
-            swiftLanguageVersions: [4])
+let package = Package(name: "Bluetooth",
+                      products: [
+                        .library(
+                            name: "Bluetooth",
+                            targets: ["Bluetooth"]
+                        )
+                      ],
+                      targets: [
+                        .target(name: "Bluetooth", path: "./Sources"),
+                        .testTarget(name: "BluetoothTests", dependencies: ["Bluetooth"])
+                      ],
+                      swiftLanguageVersions: [4])
