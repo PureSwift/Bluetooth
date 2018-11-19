60085e4cc13867abe17879a7b08f9591bda50e72
Alsey Coleman Miller <alseycmiller@gmail.com>
2018-11-18 21:52:29 -0500

    #115 Updated for Swift 4.2

--- Sources/Data.swift
+++ Sources/Data.swift
@@ -10,12 +10,23 @@ import Foundation
 
 internal extension Data {
     
+    #if swift(>=4.2)
+    func subdataNoCopy(in range: Range<Int>) -> Data {
+    
+        let pointer = withUnsafeBytes { UnsafeMutableRawPointer(mutating: $0).advanced(by: range.lowerBound) }
+        return Data(bytesNoCopy: pointer, count: range.count, deallocator: .none)
+    }
+    #else
     func subdataNoCopy(in range: CountableRange<Int>) -> Data {
         
         let pointer = withUnsafeBytes { UnsafeMutableRawPointer(mutating: $0).advanced(by: range.lowerBound) }
-        
         return Data(bytesNoCopy: pointer, count: range.count, deallocator: .none)
     }
+    
+    func subdata(in range: CountableRange<Int>) -> Data {
+        return Data(self[range])
+    }
+    #endif
     
     func suffixNoCopy(from index: Int) -> Data {
         
