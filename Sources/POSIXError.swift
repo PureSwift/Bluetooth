7a9dacd8f88ad6905aa94b0a35737fd206d18301
Alsey Coleman Miller <alseycmiller@gmail.com>
2019-04-14 16:22:24 -0500

    Fixed `POSIXError` for Linux
    Should not use `POSIXError.Code` on Linux since it is currently broken.
    
    https://github.com/apple/swift-corelibs-foundation/pull/2113

--- Sources/POSIXError.swift
+++ Sources/POSIXError.swift
@@ -13,9 +13,35 @@ internal extension POSIXError {
     /// Creates error from C ```errno```.
     static var fromErrno: POSIXError? {
         
-        guard let code = POSIXError.Code(rawValue: POSIXError.Code.RawValue(errno))
+        let errorCode = errno
+        
+        #if canImport(Darwin)
+        guard let code = POSIXError.Code(rawValue: POSIXError.Code.RawValue(errorCode))
             else { return nil }
         
         return self.init(code)
+        #else
+        guard errorCode != 0
+            else { return nil }
+        
+        self.init(errorCode)
+        #endif
+    }
+    
+    /// Initializes `POSIXError` from error code.
+    init(_ errorCode: Int32) {
+        
+        var userInfo = [String: Any](minimumCapacity: 1)
+        
+        if let description = String(cString: strerror(errorCode), encoding: .ascii) {
+            userInfo[NSLocalizedDescriptionKey] = description
+        }
+        
+        let nsError = NSError(
+            domain: NSPOSIXErrorDomain,
+            code: Int(errorCode),
+            userInfo: userInfo)
+        
+        self.init(_nsError: nsError)
     }
 }
