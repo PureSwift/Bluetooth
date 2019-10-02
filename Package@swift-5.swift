10a9242f640c4ca773d434fb0c04f466ee240937
Alsey Coleman Miller <alseycmiller@gmail.com>
2019-10-01 23:54:44 -0500

    Build dynamic framework with SwiftPM

--- Package@swift-5.swift
+++ Package@swift-5.swift
@@ -1,15 +1,18 @@
 // swift-tools-version:5.0
 import PackageDescription
 
-let package = Package(name: "Bluetooth",
-                      products: [
-                        .library(
-                            name: "Bluetooth",
-                            targets: ["Bluetooth"]
-                        )
-                      ],
-                      targets: [
-                        .target(name: "Bluetooth", path: "./Sources"),
-                        .testTarget(name: "BluetoothTests", dependencies: ["Bluetooth"])
-                      ],
-                      swiftLanguageVersions: [.v4, .v4_2, .v5])
+let package = Package(
+    name: "Bluetooth",
+    products: [
+        .library(
+            name: "Bluetooth",
+            type: .dynamic,
+            targets: ["Bluetooth"]
+        )
+    ],
+    targets: [
+        .target(name: "Bluetooth", path: "./Sources"),
+        .testTarget(name: "BluetoothTests", dependencies: ["Bluetooth"])
+    ],
+    swiftLanguageVersions: [.v5]
+)
