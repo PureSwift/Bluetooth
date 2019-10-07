b03524c7a0c7eca048133a9df9dc4bbe727b38e0
Alsey Coleman Miller <alseycmiller@gmail.com>
2019-10-07 00:53:39 -0500

    Build dynamic framework on Linux

--- Package@swift-5.swift
+++ Package@swift-5.swift
@@ -1,12 +1,18 @@
 // swift-tools-version:5.0
 import PackageDescription
 
+#if os(Linux)
+let libraryType: PackageDescription.Product.Library.LibraryType = .dynamic
+#else
+let libraryType: PackageDescription.Product.Library.LibraryType = .static
+#endif
+
 let package = Package(
     name: "Bluetooth",
     products: [
         .library(
             name: "Bluetooth",
-            type: .dynamic,
+            type: libraryType,
             targets: ["Bluetooth"]
         )
     ],
