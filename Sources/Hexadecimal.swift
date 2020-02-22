97aeb1e870a51108e9f61a5435f5f58622d31a82
Alsey Coleman Miller <alseycmiller@gmail.com>
2020-02-22 11:21:42 -0500

    Updated hexadecimal extension

--- Sources/Hexadecimal.swift
+++ Sources/Hexadecimal.swift
@@ -6,65 +6,14 @@
 //  Copyright © 2016 PureSwift. All rights reserved.
 //
 
-internal extension UInt8 {
+internal extension FixedWidthInteger {
     
     func toHexadecimal() -> String {
         
         var string = String(self, radix: 16)
-        
-        if string.utf8.count == 1 {
-            
+        while string.utf8.count < (MemoryLayout<Self>.size * 2) {
             string = "0" + string
         }
-        
-        return string.uppercased()
-    }
-}
-
-internal extension UInt16 {
-    
-    func toHexadecimal() -> String {
-        
-        var string = String(self, radix: 16)
-        
-        while string.utf8.count < (MemoryLayout<UInt16>.size * 2) {
-            
-            // prepend zeroes
-            string = "0" + string
-        }
-        
-        return string.uppercased()
-    }
-}
-
-internal extension UInt32 {
-    
-    func toHexadecimal() -> String {
-        
-        var string = String(self, radix: 16)
-        
-        while string.utf8.count < (MemoryLayout<UInt32>.size * 2) {
-            
-            // prepend zeroes
-            string = "0" + string
-        }
-        
-        return string.uppercased()
-    }
-}
-
-internal extension UInt64 {
-    
-    func toHexadecimal() -> String {
-        
-        var string = String(self, radix: 16)
-        
-        while string.utf8.count < (MemoryLayout<UInt64>.size * 2) {
-            
-            // prepend zeroes
-            string = "0" + string
-        }
-        
         return string.uppercased()
     }
 }
