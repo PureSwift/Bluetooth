4bc07374e208500ca796967205af827c7e551a78
Alsey Coleman Miller <alseycmiller@gmail.com>
2022-04-25 06:05:41 -0700

    Updated documentation script

--- generate-docs.sh
+++ generate-docs.sh
@@ -4,10 +4,9 @@ mkdir -p "./docs"
 echo "Build"
 swift build
 echo "Generate HTML"
-swift package --allow-writing-to-directory "./docs" \
-    generate-documentation --target Bluetooth --disable-indexing \
-    --output-path "./docs" \
+swift package --allow-writing-to-directory ./docs \
+    generate-documentation --target Bluetooth \
+    --disable-indexing \
     --transform-for-static-hosting \
-    --hosting-base-path "./"
-
-rm -rf .build
+    --hosting-base-path Bluetooth \
+    --output-path ./docs
