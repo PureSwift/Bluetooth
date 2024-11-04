836db2f4e4ec01d22f6fcb4db36989b6f8791cc6
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-11-04 13:58:08 -0500

    #158 Add Swift StdLib `UInt128` support

--- Sources/Bluetooth/UInt128.swift
+++ Sources/Bluetooth/UInt128.swift
@@ -9,92 +9,26 @@
 import Foundation
 #endif
 
-/// A 128 bit number stored according to host endianness.
-///
-/// Unlike `NSUUID` which is always stored in big endian.
-@frozen
-public struct UInt128: ByteValue, Sendable {
+// MARK: - ByteValue
+
+extension UInt128: ByteValue {
     
     public typealias ByteValue = (UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8)
-    
-    public static var bitWidth: Int { return 128 }
-    
-    public var bytes: ByteValue
-    
-    public init(bytes: ByteValue = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)) {
         
-        self.bytes = bytes
+    public var bytes: ByteValue {
+      @_transparent
+      get {
+          unsafeBitCast(self, to: ByteValue.self)
+      }
+    
+      @_transparent
+      set {
+          self = .init(bytes: newValue)
+      }
     }
-}
-
-public extension UInt128 {
     
-    /// The minimum representable value in this type.
-    static var min: UInt128 { return UInt128(bytes: (.min, .min, .min, .min, .min, .min, .min, .min, .min, .min, .min, .min, .min, .min, .min, .min)) }
-    
-    /// The maximum representable value in this type.
-    static var max: UInt128 { return UInt128(bytes: (.max, .max, .max, .max, .max, .max, .max, .max, .max, .max, .max, .max, .max, .max, .max, .max)) }
-    
-    /// The value with all bits set to zero.
-    static var zero: UInt128 { return .min }
-}
-
-// MARK: - Equatable
-
-extension UInt128: Equatable {
-    
-    public static func == (lhs: UInt128, rhs: UInt128) -> Bool {
-        
-        return lhs.bytes.0 == rhs.bytes.0 &&
-                lhs.bytes.1 == rhs.bytes.1 &&
-                lhs.bytes.2 == rhs.bytes.2 &&
-                lhs.bytes.3 == rhs.bytes.3 &&
-                lhs.bytes.4 == rhs.bytes.4 &&
-                lhs.bytes.5 == rhs.bytes.5 &&
-                lhs.bytes.6 == rhs.bytes.6 &&
-                lhs.bytes.7 == rhs.bytes.7 &&
-                lhs.bytes.8 == rhs.bytes.8 &&
-                lhs.bytes.9 == rhs.bytes.9 &&
-                lhs.bytes.10 == rhs.bytes.10 &&
-                lhs.bytes.11 == rhs.bytes.11 &&
-                lhs.bytes.12 == rhs.bytes.12 &&
-                lhs.bytes.13 == rhs.bytes.13 &&
-                lhs.bytes.14 == rhs.bytes.14 &&
-                lhs.bytes.15 == rhs.bytes.15
-    }
-}
-
-// MARK: - Hashable
-
-extension UInt128: Hashable {
-    
-    public func hash(into hasher: inout Hasher) {
-        withUnsafeBytes(of: bytes) { hasher.combine(bytes: $0) }
-    }
-}
-
-// MARK: - CustomStringConvertible
-
-extension UInt128: CustomStringConvertible {
-    
-    public var description: String {
-        let bytes = self.bigEndian.bytes
-        return bytes.0.toHexadecimal()
-            + bytes.1.toHexadecimal()
-            + bytes.2.toHexadecimal()
-            + bytes.3.toHexadecimal()
-            + bytes.4.toHexadecimal()
-            + bytes.5.toHexadecimal()
-            + bytes.6.toHexadecimal()
-            + bytes.7.toHexadecimal()
-            + bytes.8.toHexadecimal()
-            + bytes.9.toHexadecimal()
-            + bytes.10.toHexadecimal()
-            + bytes.11.toHexadecimal()
-            + bytes.12.toHexadecimal()
-            + bytes.13.toHexadecimal()
-            + bytes.14.toHexadecimal()
-            + bytes.15.toHexadecimal()
+    public init(bytes: ByteValue) {
+        self = unsafeBitCast(bytes, to: Self.self)
     }
 }
 
@@ -126,30 +60,9 @@ public extension UInt128 {
 
 // MARK: - Byte Swap
 
-extension UInt128: ByteSwap {
-    
-    /// A representation of this integer with the byte order swapped.
-    public var byteSwapped: UInt128 {
-        return UInt128(bytes: (bytes.15,
-                               bytes.14,
-                               bytes.13,
-                               bytes.12,
-                               bytes.11,
-                               bytes.10,
-                               bytes.9,
-                               bytes.8,
-                               bytes.7,
-                               bytes.6,
-                               bytes.5,
-                               bytes.4,
-                               bytes.3,
-                               bytes.2,
-                               bytes.1,
-                               bytes.0))
-    }
-}
+extension UInt128: ByteSwap { }
 
-// MARK: - NSUUID
+// MARK: - UUID
 
 public extension UInt128 {
     
@@ -187,12 +100,245 @@ public extension UUID {
     }
 }
 
-// MARK: - ExpressibleByIntegerLiteral
+// MARK: - Backwards compatibility
 
-extension UInt128: ExpressibleByIntegerLiteral {
+#if canImport(Darwin)
+/// A 128-bit signed integer value type.
+@frozen
+public struct UInt128: Sendable {
     
-    public init(integerLiteral value: UInt64) {
-        let bytes = value.bigEndian.bytes
-        self = UInt128(bigEndian: UInt128(bytes: (0, 0, 0, 0, 0, 0, 0, 0, bytes.0, bytes.1, bytes.2, bytes.3, bytes.4, bytes.5, bytes.6, bytes.7)))
+#if _endian(little)
+  public var _low: UInt64
+  public var _high: UInt64
+#else
+  public var _high: UInt64
+  public var _low: UInt64
+#endif
+
+  @_transparent
+  public init(_low: UInt64, _high: UInt64) {
+    self._low = _low
+    self._high = _high
+  }
+  
+  // Allow conversion if type is available.
+  @available(macOS 15, iOS 18, watchOS 11, tvOS 18, visionOS 2, *)
+  public var _value: Int128 {
+    @_transparent
+    get {
+      unsafeBitCast(self, to: Int128.self)
+    }
+
+    @_transparent
+    set {
+      self = Self(newValue)
+    }
+  }
+  
+  @available(macOS 15, iOS 18, watchOS 11, tvOS 18, visionOS 2, *)
+  @_transparent
+  public init(_ _value: Int128) {
+    self = unsafeBitCast(_value, to: Self.self)
+  }
+
+  /// Creates a new instance with the same memory representation as the given
+  /// value.
+  ///
+  /// This initializer does not perform any range or overflow checking. The
+  /// resulting instance may not have the same numeric value as
+  /// `bitPattern`---it is only guaranteed to use the same pattern of bits in
+  /// its binary representation.
+  ///
+  /// - Parameter bitPattern: A value to use as the source of the new instance's
+  ///   binary representation.
+  @available(macOS 15, iOS 18, watchOS 11, tvOS 18, visionOS 2, *)
+  @_transparent
+  public init(bitPattern: Int128) {
+    self.init(bitPattern)
+  }
+}
+
+@available(macOS 15, iOS 18, watchOS 11, tvOS 18, visionOS 2, *)
+public extension Bluetooth.UInt128 {
+    
+    init(_ value: Swift.UInt128) {
+        self.init(_low: value._low, _high: value._high)
     }
 }
+
+@available(macOS 15, iOS 18, watchOS 11, tvOS 18, visionOS 2, *)
+public extension Swift.UInt128 {
+    
+    init(_ value: Bluetooth.UInt128) {
+        self.init(bitPattern: value._value)
+    }
+}
+
+// MARK: - Constants
+
+extension Bluetooth.UInt128 {
+  @_transparent
+  public static var zero: Self {
+      Self(_low: .min, _high: .min)
+  }
+
+  @_transparent
+  public static var min: Self {
+    zero
+  }
+
+  @_transparent
+  public static var max: Self {
+    Self(_low: .max, _high: .max)
+  }
+}
+
+// MARK: - ExpressibleByIntegerLiteral
+
+@available(macOS 15, iOS 18, watchOS 11, tvOS 18, visionOS 2, *)
+extension Bluetooth.UInt128: ExpressibleByIntegerLiteral {
+    
+    public typealias IntegerLiteralType = Swift.UInt128
+    
+    public init(integerLiteral value: Swift.UInt128) {
+        self.init(_low: value._low, _high: value._high)
+    }
+}
+
+// MARK: - Conversions
+
+extension Bluetooth.UInt128 {
+
+  @inlinable
+  public init?<T>(exactly source: T) where T: BinaryInteger {
+    guard let high = UInt64(exactly: source >> 64) else { return nil }
+    let low = UInt64(truncatingIfNeeded: source)
+    self.init(_low: low, _high: high)
+  }
+
+  @inlinable
+  public init<T>(_ source: T) where T: BinaryInteger {
+    guard let value = Self(exactly: source) else {
+      fatalError("value cannot be converted to UInt128 because it is outside the representable range")
+    }
+    self = value
+  }
+
+  @inlinable
+  public init<T>(clamping source: T) where T: BinaryInteger {
+    guard let value = Self(exactly: source) else {
+      self = source < .zero ? .zero : .max
+      return
+    }
+    self = value
+  }
+
+  @inlinable
+  public init<T>(truncatingIfNeeded source: T) where T: BinaryInteger {
+    let high = UInt64(truncatingIfNeeded: source >> 64)
+    let low = UInt64(truncatingIfNeeded: source)
+    self.init(_low: low, _high: high)
+  }
+
+  @_transparent
+  public init(_truncatingBits source: UInt) {
+    self.init(_low: UInt64(source), _high: .zero)
+  }
+}
+
+extension UInt128 {
+  @inlinable
+  public init?<T>(exactly source: T) where T: BinaryFloatingPoint {
+    let highAsFloat = (source * 0x1.0p-64).rounded(.towardZero)
+    guard let high = UInt64(exactly: highAsFloat) else { return nil }
+    guard let low = UInt64(
+      exactly: high == 0 ? source : source - 0x1.0p64*highAsFloat
+    ) else { return nil }
+    self.init(_low: low, _high: high)
+  }
+
+  @inlinable
+  public init<T>(_ source: T) where T: BinaryFloatingPoint {
+    guard let value = Self(exactly: source.rounded(.towardZero)) else {
+      fatalError("value cannot be converted to UInt128 because it is outside the representable range")
+    }
+    self = value
+  }
+}
+
+// MARK: - Equatable
+
+extension UInt128: Equatable {
+    
+    public static func == (lhs: UInt128, rhs: UInt128) -> Bool {
+        return lhs._low == rhs._low
+            && lhs._high == rhs._high
+    }
+}
+
+// MARK: - Hashable
+
+extension UInt128: Hashable {
+    
+    @inlinable
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(_low)
+        hasher.combine(_high)
+    }
+}
+
+// MARK: - Comparable
+
+@available(macOS 15, iOS 18, watchOS 11, tvOS 18, visionOS 2, *)
+extension UInt128: Comparable {
+    
+    @_transparent
+    public static func < (lhs: Self, rhs: Self) -> Bool {
+        lhs._value < rhs._value
+    }
+}
+
+// MARK: - CustomStringConvertible
+
+extension UInt128: CustomStringConvertible {
+    
+    public var description: String {
+        if #available(macOS 15, iOS 18, watchOS 11, tvOS 18, visionOS 2, *) {
+            return Swift.UInt128(self).description
+        } else {
+            let bytes = self.bigEndian.bytes
+            return bytes.0.toHexadecimal()
+                + bytes.1.toHexadecimal()
+                + bytes.2.toHexadecimal()
+                + bytes.3.toHexadecimal()
+                + bytes.4.toHexadecimal()
+                + bytes.5.toHexadecimal()
+                + bytes.6.toHexadecimal()
+                + bytes.7.toHexadecimal()
+                + bytes.8.toHexadecimal()
+                + bytes.9.toHexadecimal()
+                + bytes.10.toHexadecimal()
+                + bytes.11.toHexadecimal()
+                + bytes.12.toHexadecimal()
+                + bytes.13.toHexadecimal()
+                + bytes.14.toHexadecimal()
+                + bytes.15.toHexadecimal()
+        }
+    }
+}
+
+// TODO: Implement Integer protocols
+extension Bluetooth.UInt128 {
+    
+    @_transparent
+    public static var bitWidth: Int { 128 }
+    
+    public var byteSwapped: Self {
+      return Self(_low: _high.byteSwapped, _high: _low.byteSwapped)
+    }
+}
+
+
+#else
+public typealias UInt128 = Swift.UInt128
+#endif
