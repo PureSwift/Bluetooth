de7dfa96f41d65f6338475280793349d4e900129
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-11-05 19:44:05 -0500

    #139 Add `OptionSet` extensions

---
+++ Sources/BluetoothGAP/Extensions/OptionSet.swift
@@ -0,0 +1,30 @@
+//
+//  OptionSet.swift
+//  Bluetooth
+//
+//  Created by Alsey Coleman Miller on 11/5/24.
+//
+
+extension OptionSet {
+  @inline(never)
+  internal func buildDescription(
+    _ descriptions: [(Element, StaticString)]
+  ) -> String {
+    var copy = self
+    var result = "["
+
+    for (option, name) in descriptions {
+      if _slowPath(copy.contains(option)) {
+        result += name.description
+        copy.remove(option)
+        if !copy.isEmpty { result += ", " }
+      }
+    }
+
+    if _slowPath(!copy.isEmpty) {
+      result += "\(Self.self)(rawValue: \(copy.rawValue))"
+    }
+    result += "]"
+    return result
+  }
+}
