1003a234a67055fabb6c30fa78c4977a11f65f9e
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-11-07 00:51:51 -0500

    #161 Add `DataConvertible` conformance to `ATTFindInformationRequest`

--- Sources/BluetoothGATT/ATTFindInformationRequest.swift
+++ Sources/BluetoothGATT/ATTFindInformationRequest.swift
@@ -6,46 +6,27 @@
 //  Copyright © 2018 PureSwift. All rights reserved.
 //
 
-import Foundation
+import Bluetooth
 
 /// Find Information Request
 ///
 /// The *Find Information Request* is used to obtain the mapping of attribute handles with their associated types.
 /// This allows a client to discover the list of attributes and their types on a server.
 @frozen
-public struct ATTFindInformationRequest: ATTProtocolDataUnit, Equatable {
+public struct ATTFindInformationRequest: ATTProtocolDataUnit, Equatable, Hashable, Sendable {
     
-    public static var attributeOpcode: ATTOpcode { return .findInformationRequest }
+    public static var attributeOpcode: ATTOpcode { .findInformationRequest }
     
     public var startHandle: UInt16
     
     public var endHandle: UInt16
     
-    public init(startHandle: UInt16,
-                endHandle: UInt16) {
-        
+    public init(
+        startHandle: UInt16,
+        endHandle: UInt16
+    ) {
         self.startHandle = startHandle
         self.endHandle = endHandle
-    }
-}
-
-public extension ATTFindInformationRequest {
-    
-    internal static var length: Int { return 5 }
-    
-    init?(data: Data) {
-        
-        guard data.count == type(of: self).length,
-            type(of: self).validateOpcode(data)
-            else { return nil }
-        
-        self.startHandle = UInt16(littleEndian: UInt16(bytes: (data[1], data[2])))
-        self.endHandle = UInt16(littleEndian: UInt16(bytes: (data[3], data[4])))
-    }
-    
-    var data: Data {
-        
-        return Data(self)
     }
 }
 
@@ -53,15 +34,25 @@ public extension ATTFindInformationRequest {
 
 extension ATTFindInformationRequest: DataConvertible {
     
-    var dataLength: Int {
+    public static var length: Int { 5 }
+    
+    public init?<Data: DataContainer>(data: Data) {
         
-        return type(of: self).length
+        guard data.count == Self.length,
+            Self.validateOpcode(data)
+            else { return nil }
+        
+        self.startHandle = UInt16(littleEndian: UInt16(bytes: (data[1], data[2])))
+        self.endHandle = UInt16(littleEndian: UInt16(bytes: (data[3], data[4])))
     }
     
-    static func += <T: DataContainer> (data: inout T, value: ATTFindInformationRequest) {
-        
-        data += attributeOpcode.rawValue
-        data += value.startHandle.littleEndian
-        data += value.endHandle.littleEndian
+    public func append<Data>(to data: inout Data) where Data : DataContainer {
+        data += Self.attributeOpcode.rawValue
+        data += self.startHandle.littleEndian
+        data += self.endHandle.littleEndian
+    }
+    
+    public var dataLength: Int {
+        Self.length
     }
 }
