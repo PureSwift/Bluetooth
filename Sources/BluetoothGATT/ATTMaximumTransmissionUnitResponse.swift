787e6bf47e2a5adb872b0ecc1b0069019cc3315c
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-11-07 20:52:52 -0500

    #161 Add `DataConvertible` conformance to `ATTMaximumTransmissionUnitResponse`

--- Sources/BluetoothGATT/ATTMaximumTransmissionUnitResponse.swift
+++ Sources/BluetoothGATT/ATTMaximumTransmissionUnitResponse.swift
@@ -6,13 +6,13 @@
 //  Copyright © 2018 PureSwift. All rights reserved.
 //
 
-import Foundation
+import Bluetooth
 
 ///  Exchange MTU Response
 ///
 /// The *Exchange MTU Response* is sent in reply to a received *Exchange MTU Request*.
 @frozen
-public struct ATTMaximumTransmissionUnitResponse: ATTProtocolDataUnit, Equatable {
+public struct ATTMaximumTransmissionUnitResponse: ATTProtocolDataUnit, Equatable, Hashable, Sendable {
     
     /// 0x03 = Exchange MTU Response
     public static var attributeOpcode: ATTOpcode { return .maximumTransmissionUnitResponse }
@@ -23,27 +23,7 @@ public struct ATTMaximumTransmissionUnitResponse: ATTProtocolDataUnit, Equatable
     public var serverMTU: UInt16
     
     public init(serverMTU: UInt16) {
-        
         self.serverMTU = serverMTU
-    }
-}
-
-public extension ATTMaximumTransmissionUnitResponse {
-    
-    internal static var length: Int { return 3 }
-    
-    init?(data: Data) {
-        
-        guard data.count == type(of: self).length,
-            type(of: self).validateOpcode(data)
-            else { return nil }
-                
-        self.serverMTU = UInt16(littleEndian: UInt16(bytes: (data[1], data[2])))
-    }
-    
-    var data: Data {
-        
-        return Data(self)
     }
 }
 
@@ -51,14 +31,23 @@ public extension ATTMaximumTransmissionUnitResponse {
 
 extension ATTMaximumTransmissionUnitResponse: DataConvertible {
     
-    var dataLength: Int {
+    public static var length: Int { 3 }
+    
+    public init?<Data: DataContainer>(data: Data) {
         
-        return type(of: self).length
+        guard data.count == Self.length,
+            Self.validateOpcode(data)
+            else { return nil }
+                
+        self.serverMTU = UInt16(littleEndian: UInt16(bytes: (data[1], data[2])))
     }
     
-    static func += <T: DataContainer> (data: inout T, value: ATTMaximumTransmissionUnitResponse) {
-        
-        data += attributeOpcode.rawValue
-        data += value.serverMTU.littleEndian
+    public func append<Data>(to data: inout Data) where Data : DataContainer {
+        data += Self.attributeOpcode.rawValue
+        data += self.serverMTU.littleEndian
+    }
+    
+    public var dataLength: Int {
+        Self.length
     }
 }
