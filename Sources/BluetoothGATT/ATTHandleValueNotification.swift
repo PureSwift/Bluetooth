ba149b761c3e743e2642cec0c6943d49856aed21
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-11-07 20:48:00 -0500

    #161 Add `DataConvertible` conformance to `ATTHandleValueNotification`

--- Sources/BluetoothGATT/ATTHandleValueNotification.swift
+++ Sources/BluetoothGATT/ATTHandleValueNotification.swift
@@ -6,64 +6,42 @@
 //  Copyright © 2018 PureSwift. All rights reserved.
 //
 
-import Foundation
+import Bluetooth
 
 /// Handle Value Notification
 ///
 /// A server can send a notification of an attribute’s value at any time.
 @frozen
-public struct ATTHandleValueNotification: ATTProtocolDataUnit, Equatable {
+public struct ATTHandleValueNotification<Value: DataContainer>: ATTProtocolDataUnit, Equatable, Hashable, Sendable {
     
-    public static var attributeOpcode: ATTOpcode { return .handleValueNotification }
+    public static var attributeOpcode: ATTOpcode { .handleValueNotification }
     
     /// The handle of the attribute.
     public var handle: UInt16
     
     /// The handle of the attribute.
-    public var value: Data
+    public var value: Value
     
-    public init(handle: UInt16, value: Data) {
-        
+    public init(handle: UInt16, value: Value) {
         self.handle = handle
         self.value = value
     }
 }
 
 public extension ATTHandleValueNotification {
-        
-    init?(data: Data) {
-        
-        guard data.count >= 3,
-            type(of: self).validateOpcode(data)
-            else { return nil }
-        
-        self.handle = UInt16(littleEndian: UInt16(bytes: (data[1], data[2])))
-        self.value = data.suffixCheckingBounds(from: 3)
-    }
     
-    var data: Data {
-        
-        return Data(self)
-    }
-}
-
-public extension ATTHandleValueNotification {
-    
-    init(attribute: GATTDatabase.Attribute, maximumTransmissionUnit: ATTMaximumTransmissionUnit) {
+    init(attribute: GATTDatabase<Value>.Attribute, maximumTransmissionUnit: ATTMaximumTransmissionUnit) {
         
         // If the attribue value is longer than (ATT_MTU-3) octets,
         // then only the first (ATT_MTU-3) octets of this attribute value
         // can be sent in a notification.
         let dataSize = Int(maximumTransmissionUnit.rawValue) - 3
         
-        let value: Data
+        let value: Value
         
         if attribute.value.count > dataSize {
-            
-            value = Data(attribute.value.prefix(dataSize))
-            
+            value = Value(attribute.value.prefix(dataSize))
         } else {
-            
             value = attribute.value
         }
         
@@ -75,15 +53,23 @@ public extension ATTHandleValueNotification {
 
 extension ATTHandleValueNotification: DataConvertible {
     
-    var dataLength: Int {
-        
-        return 3 + value.count
+    public init?<Data: DataContainer>(data: Data) {
+    
+        guard data.count >= 3,
+              Self.validateOpcode(data)
+            else { return nil }
+    
+        self.handle = UInt16(littleEndian: UInt16(bytes: (data[1], data[2])))
+        self.value = Value(data.suffixCheckingBounds(from: 3))
     }
     
-    static func += <T: DataContainer> (data: inout T, value: ATTHandleValueNotification) {
-        
-        data += attributeOpcode.rawValue
-        data += value.handle.littleEndian
-        data += value.value
+    public func append<Data>(to data: inout Data) where Data : DataContainer {
+        data += Self.attributeOpcode.rawValue
+        data += self.handle.littleEndian
+        data += self.value
+    }
+    
+    public var dataLength: Int {
+        3 + value.count
     }
 }
