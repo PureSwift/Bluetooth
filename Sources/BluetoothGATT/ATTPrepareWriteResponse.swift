47012a9514e30463a6300dd0705a7498333e121b
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-11-07 20:56:26 -0500

    #161 Add `DataConvertible` conformance to `ATTPrepareWriteResponse`

--- Sources/BluetoothGATT/ATTPrepareWriteResponse.swift
+++ Sources/BluetoothGATT/ATTPrepareWriteResponse.swift
@@ -6,15 +6,15 @@
 //  Copyright © 2018 PureSwift. All rights reserved.
 //
 
-import Foundation
+import Bluetooth
 
 /// Prepare Write Response
 /// The *Prepare Write Response* is sent in response to a received *Prepare Write Request*
 /// and acknowledges that the value has been successfully received and placed in the prepare write queue.
 @frozen
-public struct ATTPrepareWriteResponse: ATTProtocolDataUnit, Equatable {
+public struct ATTPrepareWriteResponse <Value: DataContainer> : ATTProtocolDataUnit, Equatable, Hashable, Sendable {
     
-    public static var attributeOpcode: ATTOpcode { return .preparedWriteResponse }
+    public static var attributeOpcode: ATTOpcode { .preparedWriteResponse }
     
     /// The handle of the attribute to be written.
     public var handle: UInt16
@@ -23,34 +23,16 @@ public struct ATTPrepareWriteResponse: ATTProtocolDataUnit, Equatable {
     public var offset: UInt16
     
     /// The value of the attribute to be written.
-    public var partValue: Data
+    public var partValue: Value
     
-    public init(handle: UInt16,
-                offset: UInt16,
-                partValue: Data) {
-        
+    public init(
+        handle: UInt16,
+        offset: UInt16,
+        partValue: Value
+    ) {
         self.handle = handle
         self.offset = offset
         self.partValue = partValue
-    }
-}
-
-public extension ATTPrepareWriteResponse {
-    
-    init?(data: Data) {
-        
-        guard data.count >= 5,
-            type(of: self).validateOpcode(data)
-            else { return nil }
-        
-        self.handle = UInt16(littleEndian: UInt16(bytes: (data[1], data[2])))
-        self.offset = UInt16(littleEndian: UInt16(bytes: (data[3], data[4])))
-        self.partValue = data.suffixCheckingBounds(from: 5)
-    }
-    
-    var data: Data {
-        
-        return Data(self)
     }
 }
 
@@ -58,16 +40,25 @@ public extension ATTPrepareWriteResponse {
 
 extension ATTPrepareWriteResponse: DataConvertible {
     
-    var dataLength: Int {
+    public init?<Data: DataContainer>(data: Data) {
         
-        return 5 + partValue.count
+        guard data.count >= 5,
+            Self.validateOpcode(data)
+            else { return nil }
+        
+        self.handle = UInt16(littleEndian: UInt16(bytes: (data[1], data[2])))
+        self.offset = UInt16(littleEndian: UInt16(bytes: (data[3], data[4])))
+        self.partValue = Value(data.suffixCheckingBounds(from: 5))
     }
     
-    static func += <T: DataContainer> (data: inout T, value: ATTPrepareWriteResponse) {
-        
-        data += attributeOpcode.rawValue
-        data += value.handle.littleEndian
-        data += value.offset.littleEndian
-        data += value.partValue
+    public func append<Data>(to data: inout Data) where Data : DataContainer {
+        data += Self.attributeOpcode.rawValue
+        data += self.handle.littleEndian
+        data += self.offset.littleEndian
+        data += self.partValue
+    }
+    
+    public var dataLength: Int {
+        5 + partValue.count
     }
 }
