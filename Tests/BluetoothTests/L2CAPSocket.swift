74973ace27b26e4dfdc4c49b376436a3ece4971a
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-11-14 13:18:36 -0500

    Updated unit tests

--- Tests/BluetoothTests/L2CAPSocket.swift
+++ Tests/BluetoothTests/L2CAPSocket.swift
@@ -39,7 +39,7 @@ internal final class TestL2CAPServer: L2CAPServer {
         static func canAccept(server: BluetoothAddress) -> Bool {
             lock.lock()
             defer { lock.unlock() }
-            return pendingClients[server, default: []].isEmpty
+            return pendingClients[server, default: []].isEmpty == false
         }
     }
     
