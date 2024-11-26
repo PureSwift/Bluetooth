47b7d33f1cdfb79c5eea8e02df548271bed6ddf4
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-11-26 00:38:21 -0500

    Updated `HCIVersion`

--- Sources/BluetoothHCI/HCIVersion.swift
+++ Sources/BluetoothHCI/HCIVersion.swift
@@ -11,21 +11,52 @@
 /// Bluetooth Host Controller Interface version.
 ///
 /// SeeAlso:
-@frozen
-public enum HCIVersion: UInt8, Equatable, Hashable {
+public enum HCIVersion: UInt8, Equatable, Hashable, Sendable {
     
-    case v1_0b      = 0
-    case v1_1       = 1
-    case v1_2       = 2
-    case v2_0       = 3
-    case v2_1       = 4
-    case v3_0       = 5
-    case v4_0       = 6
-    case v4_1       = 7
-    case v4_2       = 8
-    case v5_0       = 9
-    case v5_1       = 10
-    case v5_2       = 11
+    /// Bluetooth® Core Specification 1.0b (Withdrawn)
+    case v1_0b      = 0x00
+    
+    /// Bluetooth® Core Specification 1.1 (Withdrawn)
+    case v1_1       = 0x01
+    
+    /// Bluetooth® Core Specification 1.2 (Withdrawn)
+    case v1_2       = 0x02
+    
+    /// Bluetooth® Core Specification 2.0+EDR (Withdrawn)
+    case v2_0       = 0x03
+    
+    /// Bluetooth® Core Specification 2.1+EDR (Withdrawn)
+    case v2_1       = 0x04
+    
+    /// Bluetooth® Core Specification 3.0+HS (Withdrawn)
+    case v3_0       = 0x05
+    
+    /// Bluetooth® Core Specification 4.0 (Withdrawn)
+    case v4_0       = 0x06
+    
+    /// Bluetooth® Core Specification 4.1 (Deprecated)
+    case v4_1       = 0x07
+    
+    /// Bluetooth® Core Specification 4.2
+    case v4_2       = 0x08
+    
+    /// Bluetooth® Core Specification 5.0
+    case v5_0       = 0x09
+    
+    /// Bluetooth® Core Specification 5.1
+    case v5_1       = 0x0A
+    
+    ///Bluetooth® Core Specification 5.2
+    case v5_2       = 0x0B
+    
+    /// Bluetooth® Core Specification 5.3
+    case v5_3       = 0x0C
+    
+    /// Bluetooth® Core Specification 5.4
+    case v5_4       = 0x0D
+    
+    /// Bluetooth® Core Specification 6.0
+    case v6_0       = 0x0E
 }
 
 // swiftlint:enable identifier_name
@@ -49,6 +80,9 @@ extension HCIVersion: CustomStringConvertible {
         case .v5_0:      return "5.0"
         case .v5_1:      return "5.1"
         case .v5_2:      return "5.2"
+        case .v5_3:      return "5.3"
+        case .v5_4:      return "5.4"
+        case .v6_0:      return "6.0"
         }
     }
 }
