c8877facaec2914921cb8ee7ef8172796c59b67f
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-05-20 14:24:56 +0900

    move to top level from in extension and replace to OptionSet

--- Sources/BluetoothHCI/HCILEExtendedAdvertisingReport.swift
+++ Sources/BluetoothHCI/HCILEExtendedAdvertisingReport.swift
@@ -59,7 +59,7 @@ public struct HCILEExtendedAdvertisingReport<ReportData: DataContainer>: HCIEven
 
         public static var length: Int { 2 + 1 + 6 + 1 + 1 + 1 + 1 + 1 + 2 + 1 + 6 + 1 }
 
-        public let eventType: BitMaskOptionSet<EventType>
+        public let eventType: EventType
 
         public let addressType: AddressType
 
@@ -88,7 +88,7 @@ public struct HCILEExtendedAdvertisingReport<ReportData: DataContainer>: HCIEven
             guard data.count >= Report.length
             else { return nil }
 
-            let eventType = BitMaskOptionSet<EventType>(rawValue: UInt16(littleEndian: UInt16(bytes: (data[0], data[1]))))
+            let eventType = EventType(rawValue: UInt16(littleEndian: UInt16(bytes: (data[0], data[1]))))
 
             guard let addressType = AddressType(rawValue: data[2])
             else { return nil }
@@ -229,34 +229,37 @@ public struct HCILEExtendedAdvertisingReport<ReportData: DataContainer>: HCIEven
         /// No address provided (anonymous advertisement)
         case noAddressProvided = 0xFF
     }
+}
 
-    /// Event Type
-    public enum EventType: UInt16, BitMaskOption, CaseIterable {
 
+/// Event Type
+@OptionSet<UInt16>
+public struct EventType: Sendable {    
+    private enum Options: UInt16 {
         /// Connectable advertising
         case connectableAdvertising = 0b00000000_00000001
-
+        
         /// Scannable advertising
         case scannableAdvertising = 0b00000000_00000010
-
+        
         /// Directed advertising
         case directedAdvertising = 0b00000000_00000100
-
+        
         /// Scan response
         case scanResponse = 0b00000000_00001000
-
+        
         /// Legacy advertising PDUs used
         case legacyAdvertisingPDU = 0b00000000_00010000
-
+        
         /// Data status: Complete
         case dataStatusComplete = 0b00000000_00000000
-
+        
         /// Data status: Incomplete, more data to come
         case dataStatusIncompleteMoreData = 0b00000000_00100000
-
+        
         /// Data status: Incomplete, data truncated, no more to come
         case dataStatusIncompleteTruncated = 0b00000000_01000000
-
+        
         /// Data status: Reserved for future use
         case dataStatusReserved = 0b00000000_01100000
     }
