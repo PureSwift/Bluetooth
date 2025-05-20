9048ee8977774a93604cf46c3bbcafd15ba347b0
zunda.dev@gmail.com <zunda.dev@gmail.com>
2025-05-20 15:08:30 +0900

    remove OptionSetMacro

--- Sources/BluetoothHCI/OptionSetMacro.swift
+++
@@ -1,3 +0,0 @@
-@attached(member, names: named(RawValue), named(rawValue), named(`init`), arbitrary)
-@attached(extension, conformances: OptionSet)
-public macro OptionSet<RawType>() = #externalMacro(module: "SwiftMacros", type: "OptionSetMacro")
