88b0074bf613a304014ea2f606a8a2fac72d6677
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-08-01 12:23:19 -0400

    Add the CBluetooth umbrella header

---
+++ Sources/CBluetooth/include/CBluetooth.h
@@ -0,0 +1,20 @@
+//
+//  CBluetooth.h
+//  Bluetooth
+//
+//  Umbrella header for the CBluetooth module.
+//
+//  `bluetooth.h` and `uuid.h` are vendored verbatim from BlueZ 5.85
+//  (GPL-2.0-or-later, see LICENSE in this directory). The Swift ABI
+//  implementations in `BluetoothABI` are type-checked against these
+//  declarations.
+//
+
+#ifndef CBLUETOOTH_H
+#define CBLUETOOTH_H
+
+#include "bluetooth.h"
+#include "uuid.h"
+#include "cbt_internal.h"
+
+#endif /* CBLUETOOTH_H */
