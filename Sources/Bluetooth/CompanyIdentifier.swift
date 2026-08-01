1a963fc08d0d163710e91a31556fb8bd2defa622
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-08-01 12:24:06 -0400

    Gate CompanyIdentifier's metadata name lookup behind the Metadata trait

--- Sources/Bluetooth/CompanyIdentifier.swift
+++ Sources/Bluetooth/CompanyIdentifier.swift
@@ -50,7 +50,7 @@ extension CompanyIdentifier: ExpressibleByIntegerLiteral {
 extension CompanyIdentifier: CustomStringConvertible {
 
     public var description: String {
-        #if canImport(Foundation) && canImport(BluetoothMetadata) && !os(WASI) && !hasFeature(Embedded)
+        #if Metadata && canImport(Foundation) && !os(WASI) && !hasFeature(Embedded)
         return name ?? rawValue.description
         #else
         return rawValue.description
